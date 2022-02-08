938d06b7f227fac4b4fb9810b5ad7886308d1916
Yasa Zaheen <yasazaheen@gmail.com>
2022-02-08 14:59:33 +0600

    Beta test: Active status

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+// React JS
+import { useEffect } from "react";
+
 // NEXT JS
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 
 // Firebase
-import { auth } from "../firebase";
+import firebase from "firebase";
+import { auth, db } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 // Components / Pages
@@ -13,6 +17,21 @@ import Loading from "./Loading";
 
 function MyApp({ Component, pageProps }) {
   const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users").doc(user.uid).set(
+        {
+          uid: user.uid,
+          username: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        },
+        { merge: true }
+      );
+    }
+  }, [user]);
 
   if (user)
     return (
--- pages/chat.js
+++ pages/chat.js
@@ -2,6 +2,7 @@
 import { useRef, useState, useEffect } from "react";
 
 // Next
+import Image from "next/image";
 import { useTheme } from "next-themes";
 
 // Firebase
@@ -63,6 +64,9 @@ function Chat() {
   const [formValue, setFormValue] = useState("");
 
   const user = auth.currentUser;
+  const usersCollection = db.collection("users");
+  const usersDocuments = usersCollection.orderBy("lastSeen", "desc");
+  const [usersSnapshot] = useCollectionData(usersDocuments);
 
   const messagesCollection = db.collection("messages");
   const messagesDocuments = messagesCollection
@@ -80,6 +84,17 @@ function Chat() {
 
     if (formValue != "") {
       const { uid, photoURL } = user;
+
+      db.collection("users").doc(user.uid).set(
+        {
+          uid: user.uid,
+          username: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        },
+        { merge: true }
+      );
 
       await messagesCollection.add({
         username: user.displayName,
@@ -106,49 +121,78 @@ function Chat() {
       });
   };
 
-  return (
-    <div className="h-screen w-full relative flex flex-col">
-      <div className="h-fit w-full bg-white dark:bg-black z-50 flex justify-center items-center overflow-hidden p-4 relative border-b-2 border-neutral-50 dark:border-neutral-800">
-        {renderThemeChanger()}
-        <p className="text-2xl font-semibold">hyperchat.</p>
-        {/* Header */}
-        <button
-          onClick={signOut}
-          className="absolute right-4 bg-[#007aff] dark:bg-[#ff2d55] text-white font-semibold text-sm py-2 px-4 rounded-lg flex justify-center items-center"
-        >
-          <LogoutIcon className="h-5 w-5 mr-2" />
-          logout
-        </button>
-      </div>
+  const activeStatus = (user) => {
+    const userLastSeen = user.lastSeen?.toDate();
+    const dateObject = new Date();
+    const currentTime = dateObject;
+    const timeDelta = userLastSeen - currentTime;
 
-      {/* Chat Area */}
-      <div className="flex flex-col-reverse flex-1 max-h-fit p-4 overflow-scroll overflow-x-hidden">
-        <div ref={scroller}></div>
-        {messagesSnapshot &&
-          messagesSnapshot.map((message) => (
-            <Message key={message.id} message={message} />
+    if (timeDelta > 300000) {
+      return "Inactive";
+    } else {
+      return "Active";
+    }
+  };
+
+  return (
+    <div className="h-screen w-full relative flex">
+      <div className="hidden lg:block w-1/3 p-4">
+        {usersSnapshot &&
+          usersSnapshot.map((user) => (
+            <div className="duration-200 ease-in-out flex items-center cursor-pointer hover:bg-neutral-50 hover:dark:bg-neutral-800 p-4 rounded-lg">
+              <div className="overflow-hidden h-12 w-12 rounded-full relative">
+                <Image src={user.photoURL} layout="fill" objectFit="cover" />
+              </div>
+              <div className="ml-4">
+                <p className="text-md">{user.username}</p>
+                <p className="text-xs font-thin ">{activeStatus(user)}</p>
+              </div>
+            </div>
           ))}
       </div>
-      {/* Input Area */}
-      <form
-        onSubmit={sendMessage}
-        className="w-full flex items-center p-4 border-t-2 border-neutral-50 dark:border-neutral-800"
-      >
-        <input
-          type="text"
-          value={formValue}
-          onChange={(e) => {
-            setFormValue(e.target.value);
-          }}
-          maxLength="255"
-          placeholder="Message"
-          className="flex-1 bg-neutral-50 px-4 py-2 outline-none rounded-lg dark:bg-neutral-800"
-        />
-        <IconButton
-          Icon={ArrowCircleUpIcon}
-          className="bg-[#007aff] dark:bg-[#ff2d55] text-white ml-4 h-10 w-10 p-2"
-        />
-      </form>
+      <div className="h-screen w-full relative flex flex-col">
+        <div className="h-fit w-full bg-white dark:bg-black z-50 flex justify-center items-center overflow-hidden p-4 relative border-b-2 border-neutral-50 dark:border-neutral-800">
+          {renderThemeChanger()}
+          <p className="text-2xl font-semibold">hyperchat.</p>
+          {/* Header */}
+          <button
+            onClick={signOut}
+            className="absolute right-4 bg-[#007aff] dark:bg-[#ff2d55] text-white font-semibold text-sm py-2 px-4 rounded-lg flex justify-center items-center"
+          >
+            <LogoutIcon className="h-5 w-5 mr-2" />
+            logout
+          </button>
+        </div>
+
+        {/* Chat Area */}
+        <div className="flex flex-col-reverse flex-1 max-h-fit p-4 overflow-scroll overflow-x-hidden">
+          <div ref={scroller}></div>
+          {messagesSnapshot &&
+            messagesSnapshot.map((message) => (
+              <Message key={message.id} message={message} />
+            ))}
+        </div>
+        {/* Input Area */}
+        <form
+          onSubmit={sendMessage}
+          className="w-full flex items-center p-4 border-t-2 border-neutral-50 dark:border-neutral-800"
+        >
+          <input
+            type="text"
+            value={formValue}
+            onChange={(e) => {
+              setFormValue(e.target.value);
+            }}
+            maxLength="255"
+            placeholder="Message"
+            className="flex-1 bg-neutral-50 px-4 py-2 outline-none rounded-lg dark:bg-neutral-800"
+          />
+          <IconButton
+            Icon={ArrowCircleUpIcon}
+            className="bg-[#007aff] dark:bg-[#ff2d55] text-white ml-4 h-10 w-10 p-2"
+          />
+        </form>
+      </div>
     </div>
   );
 }
