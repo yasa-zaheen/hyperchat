0d6c109626e6bdd9c7009aa3c0e4d22b6566adec
Yasa Zaheen <yasazaheen@gmail.com>
2022-02-09 06:41:58 +0600

    [Feature]: user last seen

--- components/UserCard.js
+++ components/UserCard.js
@@ -9,28 +9,36 @@ function UserCard({ user }) {
     const timeDelta = userLastSeen - currentTime;
 
     if (timeDelta < -150000) {
-      return (
-        <p className="text-xs font-light border-2 border-neutral-100 dark:border-neutral-800 text-neutral-800 dark:text-white rounded-md w-fit px-2 py-1">
-          Inactive
-        </p>
-      );
+      return null;
     } else {
-      return (
-        <p className="text-xs font-light border-2 border-[#32c7592f] text-[#32c759] rounded-md w-fit px-2 py-1">
-          Active
-        </p>
-      );
+      return <div className="h-2 w-2 bg-[#32c759] rounded-full ml-2"></div>;
+    }
+  };
+
+  const showLastSeen = (user) => {
+    if (user.lastSeen) {
+      const dateTimeObject = user.lastSeen.toDate();
+      return `${dateTimeObject.getDate()} / ${
+        dateTimeObject.getMonth() + 1
+      } / ${dateTimeObject.getFullYear()} at ${dateTimeObject.getHours()}:${
+        dateTimeObject.getMinutes() < 10
+          ? `0${dateTimeObject?.getMinutes()}`
+          : dateTimeObject?.getMinutes()
+      }`;
     }
   };
 
   return (
-    <div className="duration-200 ease-in-out flex items-center cursor-pointer hover:bg-neutral-50 hover:dark:bg-neutral-800 p-4 rounded-lg">
+    <div className="duration-200 ease-in-out flex items-center cursor-pointer hover:bg-neutral-50 hover:dark:bg-neutral-800 p-4 rounded-lg hover:scale-105">
       <div className="overflow-hidden h-12 w-12 rounded-full relative">
         <Image src={user.photoURL} layout="fill" objectFit="cover" />
       </div>
-      <div className="ml-4">
-        <p className="text-md">{user.username}</p>
-        {activeStatus(user)}
+      <div className="ml-4 flex flex-col">
+        <p className="text-md mr-2 flex items-center">
+          {user.username}
+          {activeStatus(user)}
+        </p>
+        <p className="text-xs opacity-50">Last seen at {showLastSeen(user)}</p>
       </div>
     </div>
   );
